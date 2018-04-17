f178c965cac97fb4080653200cb3f25c77152ccf
Osvaldo Bosquez <osvaldobosquez@gmail.com>
2018-04-16 19:12:56 -0600

    Initial commit

---
+++ src/cuestionario/Cuestionario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuestionario;
+
+/**
+ *
+ * @author Osvaldo Bosquez
+ */
+public class Cuestionario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pregunta1 p1 = new Pregunta1();
+        
+        p1.setLocationRelativeTo(null);
+        p1.setVisible(true);
+    }
+    
+}
---
+++ src/cuestionario/Intermediario.java
@@ -0,0 +1,23 @@
+
+package cuestionario;
+
+import java.awt.Color;
+
+public class Intermediario {
+    public static String nombre;
+    public static String compania;
+    public static String comida;
+    public static String sexo;
+    public static String edad;
+    public static String vegetales;
+    public static Color color;
+    public static String carro;
+    public static String contra;
+    public static String año;
+    public static String si;
+    public static String mascota;
+    public static String cerveza;
+    public static String date;
+    public static String pelis;
+    
+}
